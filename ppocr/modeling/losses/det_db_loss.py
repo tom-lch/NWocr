52aeace784017efe4ed1ed31cc772274210fa899
Samuel <lichao3335390>
2021-01-03 18:36:28 +0800

    change bug'

--- ppocr/modeling/losses/det_db_loss.py
+++ ppocr/modeling/losses/det_db_loss.py
@@ -16,7 +16,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .det_basic_loss import BalanceLoss, MaskL1Loss, DiceLoss, CosSimLoss
+from .det_basic_loss import BalanceLoss, MaskL1Loss, DiceLoss
 
 
 class DBLoss(object):
